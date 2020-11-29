c84697445bc8e2c40f921265d7216deb38dfeda3
Yana Manko <yanna.manko@gmail.com>
2020-11-29 18:22:37 +0200

    12

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 // serve static files....
-app.use(express.static(`${__dirname}/dist/angular-tour-of-heroes` ));
+app.use(express.static(`${__dirname}/dist/tur herous/angular-tour-of-heroes` ));
 // send all requests to index.html
 app.get('/*', (req, res) => {
-res.sendFile(path.join(`${__dirname}/dist/angular-tour-of-heroes/index.html` ));
+res.sendFile(path.join(`${__dirname}/dist/tur herous/angular-tour-of-heroes/index.html` ));
 });
 // default Heroku PORT
 app.listen(process.env.PORT || 3000);
